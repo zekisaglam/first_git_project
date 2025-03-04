ada6ae313c0e363280c02186f9e86fb3b8d5fc2b
zekisaglam <zzekisaglam@gmail.com>
2025-03-04 00:44:59 -0500

    HelloCydeo updated

--- src/day01/HelloCydeo.java
+++ src/day01/HelloCydeo.java
@@ -2,6 +2,6 @@ package day01;
 
 public class HelloCydeo {
     public static void main(String[] args) {
-        System.out.println("Hello Cydeo");
+        System.out.println("Hello Cydeooo");
     }
 }
