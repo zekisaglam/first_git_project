e2050f751de3f38725786f9edc4593f7e91048db
Zeki Saglam <64930322+zekisaglam@users.noreply.github.com>
2025-05-08 17:17:01 -0400

    Update HelloWorld.java

--- src/day01/HelloWorld.java
+++ src/day01/HelloWorld.java
@@ -3,5 +3,8 @@ package day01;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("make");
+        System.out.println("change ");
+        System.out.println("two two");
     }
 }
